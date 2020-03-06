23f953c8208394e98a710575be55fe51ff5c1eee
Keaton Mueller <keaton.mueller@yale.edu>
2020-03-05 21:23:10 -0500

    reorganized code, added beginnings of avi

---
+++ autodidactic_iteration.py
@@ -0,0 +1,118 @@
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils import data
+import random
+
+import cube as C
+from cube_net import CubeNet
+from data_generation import generate_training_data_adi
+from naive_test import naive_test
+from mcts_test import mcts_test, attempt_solve
+
+# device for CPU or GPU calculations
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+class ADI:
+    '''
+        class to implement autodidactic iteration
+    '''
+    def __init__(self, LR):
+        '''
+            initialize model and optimizer
+
+            LR: learning rate of optimizer
+        '''
+        self.model = CubeNet().to(device)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=LR)
+
+    def load(self, PATH):
+        '''
+            load model checkpoint
+
+            PATH: location of checkpoint
+        '''
+        checkpoint = torch.load(PATH, map_location=device)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.model.eval()
+        print('loaded', PATH)
+
+    def fit(self, PERIODS, EPOCHS, NUM_SCRAMBLES, SCRAMBLE_LENGTH):
+        '''
+            trains the given network with the given parameters
+        '''
+        # animated progress bar of epoch training progress
+        progress_bar_len = 18
+        # report device being trained with
+        print('training with', device)
+        # allow torch to optimize algorithms
+        torch.backends.cudnn.benchmark = True
+        # loop over the periods
+        for period in range(PERIODS):
+            print('period:', period)
+            # generate new training data
+            X, Y = generate_training_data_adi(NUM_SCRAMBLES, SCRAMBLE_LENGTH, self.model)
+
+            train_len = len(X)
+            print_len = len(str(train_len)) * 2 + 1
+
+            # enter training mode
+            self.model.train()
+            # loop over the epochs
+            for epoch in range(EPOCHS):
+                # print('\tepoch:', epoch, end='\t')
+                for i, (x, y) in enumerate(zip(X, Y)):
+                    progress = int(i / train_len * progress_bar_len)
+                    print('\tepoch:', epoch, '\t[' + str('='*progress) + '>' + str(' '*(progress_bar_len-progress)) + (']\t{0:>'+str(print_len)+'}').format(str(i)+'/'+str(train_len)), end='\r')
+                    # get expected output
+                    x, distance = x
+                    y_v, y_p = y
+
+                    # calculate network's output
+                    self.model.zero_grad()
+                    out_v, out_p = self.model(x)
+                    # out_p = torch.squeeze(out_p, dim=1)
+
+                    # compute and sum loss
+                    loss_v = F.mse_loss(out_v, y_v)
+                    loss_p = F.cross_entropy(out_p, y_p)
+                    loss = loss_v + loss_p
+                    loss = loss * (4 / (distance + 3))
+                    # update weights
+                    loss.backward()
+                    self.optimizer.step()
+
+                # report loss
+                print('\tepoch:', epoch, '\t[' + ('='*(progress_bar_len+1)) + ']\tloss:', loss.item())
+
+    def test(self, tests, SCRAMBLE_LENGTH, TIME_LIMIT):
+        '''
+            perform requested tests of the model
+
+            tests: list of test names
+            SCRAMBLE_LENGTH: length of scrambles for tests
+            TIME_LIMIT: time to attempt each solve
+        '''
+        if('naive' in tests):
+            # run a naive test
+            naive_test(self.model, SCRAMBLE_LENGTH)
+        if('mcts' in tests):
+            # run a mcts test
+            mcts_test(self.model, SCRAMBLE_LENGTH, TIME_LIMIT)
+
+    def solve(self, cube, TIME_LIMIT, scramble):
+        attempt_solve(self.model, cube, TIME_LIMIT, None, ' '.join(scramble))
+
+
+    def save(self, PATH):
+        '''
+            save model checkpoint
+
+            PATH: location to save checkpoint
+        '''
+        torch.save({
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict()
+        }, PATH)
+        print('saved model as', PATH)
--- cube_net.py
+++ cube_net.py
@@ -1,8 +1,12 @@
 import torch.nn as nn
 from torch import tanh
-from torch.nn.functional import softmax, leaky_relu
+from torch.nn.functional import softmax, leaky_relu, relu
 
 class CubeNet(nn.Module):
+    '''
+        a CubeNet network used for the autodidactic iteration approach
+        to solving a Rubik's Cube via reinforcement learning
+    '''
     def __init__(self):
         super().__init__()
 
@@ -38,3 +42,68 @@ class CubeNet(nn.Module):
         out_p = softmax(self.out_p(x_p), dim=1)
 
         return (out_v, out_p)
+
+class ResCubeNet(nn.Module):
+    '''
+        a Residual CubeNet network used for the value iteration approach
+        to solving a Rubik's Cube via reinforcement learning
+    '''
+    def __init__(self):
+        super().__init__()
+
+        self.fc1 = nn.Linear(20*24, 5000)        # first fully connected layer
+        self.bn1 = nn.BatchNorm1d(5000)          # batch normalization layer
+        self.fc2 = nn.Linear(5000, 1000)         # second fully connected layer
+        self.bn2 = nn.BatchNorm1d(1000)          # batch normalization layer
+
+        # layers for first residual block
+        self.res1a = nn.Linear(1000, 1000)
+        self.res1a_bn = nn.BatchNorm1d(1000)
+        self.res1b = nn.Linear(1000, 1000)
+        self.res1b_bn = nn.BatchNorm1d(1000)
+
+        # layers for second residual block
+        self.res2a = nn.Linear(1000, 1000)
+        self.res2a_bn = nn.BatchNorm1d(1000)
+        self.res2b = nn.Linear(1000, 1000)
+        self.res2b_bn = nn.BatchNorm1d(1000)
+
+        # layers for third residual block
+        self.res3a = nn.Linear(1000, 1000)
+        self.res3a_bn = nn.BatchNorm1d(1000)
+        self.res3b = nn.Linear(1000, 1000)
+        self.res3b_bn = nn.BatchNorm1d(1000)
+
+        # layers for fourth residual block
+        self.res4a = nn.Linear(1000, 1000)
+        self.res4a_bn = nn.BatchNorm1d(1000)
+        self.res4b = nn.Linear(1000, 1000)
+        self.res4b_bn = nn.BatchNorm1d(1000)
+
+        # identity layer for residual connections
+        self.identity = nn.Identity(1000)
+
+    def forward(self, x):
+        # first two hidden layers
+        x = relu(self.bn1(self.fc1(x)))
+        x = relu(self.bn2(self.fc2(x)))
+
+        # first residual block
+        residual = self.identity(x)
+        x = relu(self.res1a_bn(self.res1a(x)))
+        x = relu(self.res1b_bn(self.res1b(x)) + residual)
+
+        # second residual block
+        residual = self.identity(x)
+        x = relu(self.res2a_bn(self.res2a(x)))
+        x = relu(self.res2b_bn(self.res2b(x)) + residual)
+
+        # third residual block
+        residual = self.identity(x)
+        x = relu(self.res3a_bn(self.res3a(x)))
+        x = relu(self.res3b_bn(self.res3b(x)) + residual)
+
+        # fourth residual block
+        residual = self.identity(x)
+        x = relu(self.res4a_bn(self.res4a(x)))
+        return relu(self.res4b_bn(self.res4b(x)) + residual)
