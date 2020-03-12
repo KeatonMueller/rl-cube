0996fd4265f1320d78cce73c2eafb4e0ad98220e
KeatonMueller <kmueller135@gmail.com>
2020-03-12 17:43:33 -0400

    renamed approximate_value_iteration

--- approximate_value_iteration.py
+++ approximate_value_iteration.py
@@ -19,8 +19,8 @@ class AVI:
         class to implement approximate value iteration
     '''
     def __init__(self):
-        self.model_train = ResCubeNet()
-        self.model_label = ResCubeNet()
+        self.model_train = ResCubeNet().to(device)
+        self.model_label = ResCubeNet().to(device)
         self.model_label.load_state_dict(self.model_train.state_dict())
 
         self.optim_train = optim.Adam(self.model_train.parameters(), lr=0.01)
@@ -32,8 +32,18 @@ class AVI:
     def cubes_to_input(self, samples):
         '''
             converts a list of Cube objects into a pytorch tensor for input into a network
+
+            samples: list of Cube objects that are training examples
         '''
-        pass
+        # empty tensor to store input
+        X = torch.empty(len(samples), 480)
+        # for each cube
+        for i, cube in enumerate(samples):
+            # mark this state as seen
+            self.seen_states.add(cube)
+            # convert it to a tensor
+            X[i] = cube.to_tensor()
+        return X
 
     def label_samples(self, samples):
         '''
@@ -79,87 +89,51 @@ class AVI:
         # return the labels
         return Y
 
-    def get_batches(self, examples, batch_size):
+    def get_batches(self, X, Y, batch_size):
         '''
-            label the given training examples and return them in batches
+            convert given input and labels into batches
 
-            examples: list of Cube objects that are training examples
+            X: training examples
+            Y: labels
             batch_size: size of batches to create
         '''
+        # randomly shuffle the input and output
+        indices = torch.randperm(X.size()[0])
+        X = X[indices]
+        Y = Y[indices]
+
         # calculate number of batches
-        num_batches = int(len(examples) / batch_size)
-        # empty tensors for training examples and labels
-        X = torch.empty(num_batches, batch_size, 480)
-        Y = torch.empty(num_batches, batch_size, 1)
-        self.model_label.eval()
-        with torch.no_grad():
-            # for each batch of training examples
-            for b in range(num_batches):
-                # get batch of training examples in array form
-                training_batch = examples[b * batch_size : (b + 1) * batch_size]
-                # empty tensor to hold all of their next states
-                next_states = torch.empty(batch_size * 12, 480)
-                # populate next_states tensor
-                num = 0
-                # for each training example
-                for cube in training_batch:
-                    # print(str(num) + 'th cube', cube.get_array())
-                    # make all 12 possible moves and store tensor
-                    for idx in range(12):
-                        cube.idx_turn(idx)
-                        # print('\t', cube.get_array())
-                        next_states[num] = cube.to_tensor()
-                        cube.idx_turn(idx, True)
-                        num += 1
-                # get outputs
-                outputs = self.model_label(next_states)
-                # find label for each training example
-                for x in range(batch_size):
-                    min_cost = float('inf')
-                    for i in range(x * 12, x * 12 + 12):
-                        cost = 1 + outputs[i].item()
-                        if cost < min_cost:
-                            min_cost = cost
-                    # store results
-                    X[b][x] = examples[b * batch_size + x].to_tensor()
-                    Y[b][x] = min_cost
-            '''
-            for i, cube in enumerate(X):
-                # find min cost over 12 possible moves
-                min_cost = float('inf')
-                for idx in range(12):
-                    # perform a move
-                    cube.idx_turn(idx)
-                    # calculate cost
-                    cost = 1 + self.model_label(cube.to_tensor())
-                    # update min
-                    if cost < min_cost:
-                        min_cost = cost
-                    # undo the move
-                    cube.idx_turn(idx, True)
-                # record this label
-                Y[i] = min_cost
-            '''
-        return X, Y
+        num_batches = int(len(X) / batch_size)
+        # empty tensors for batched training examples and labels
+        X_batches = torch.empty(num_batches, batch_size, 480)
+        Y_batches = torch.empty(num_batches, batch_size, 1)
+
+        for b in range(num_batches):
+            x_batch = X[b * batch_size : (b + 1) * batch_size]
+            y_batch = Y[b * batch_size : (b + 1) * batch_size]
+
+            X_batches[b] = x_batch
+            Y_batches[b] = y_batch
+
+        return X_batches, Y_batches
 
     def fit(self, batches):
         pass
 
     def train(self, num_scrambles, batch_size, max_updates):
-        # ensure the number of scrambles can be split into batches evenly
+        # check that the number of scrambles can be split into batches evenly
+        # (this is up to the caller to ensure)
         if(num_scrambles % batch_size != 0):
             print(f'number of scrambles ({num_scrambles}) and batch size ({batch_size}) aren\'t compatible')
             exit()
 
         num_updates = 0
-        for i in range(10): # this should be a while(num_updates < max_updates) once things are working
+        for i in range(5): # this should be a while(num_updates < max_updates) once things are working
             samples = generate_training_data_avi(num_scrambles, 30)
 
-
-            Y = self.label_samples(samples)
             X = self.cubes_to_input(samples)
-
-            batches = self.get_batches(samples, batch_size)
+            Y = self.label_samples(samples)
+            batches = self.get_batches(X, Y, batch_size)
 
             self.fit(batches)
             if(len(self.seen_states) > STATES_PER_UPDATE):
